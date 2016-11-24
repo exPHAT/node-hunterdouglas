1b9e67152d672e56624920c951f0185ea2c07f50
ExPHAT <me@exphat.com>
2016-11-23 19:14:21 -0500

    core refactor, use babel

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
 const hd = require(".");
 
 const blindController = hd({
-  ip: "192.168.1.15",
+  ip: "192.168.1.41",
   port: 522
 });
 
-blindController.setup().then(function (commands) {
-  blindController.open("family room").then(function (data) {
+blindController.setup().then(function (rooms) {
+  console.log('rooms', rooms);
+
+  blindController.close("family room").then(function (data) {
     console.log('data', data);
   }).catch(function (err) {
     console.log('err', err);
+  }).then(function () {
+    blindController.disconnect();
   });
 });
